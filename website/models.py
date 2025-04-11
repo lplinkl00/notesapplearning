383c1aa2c17a3fd602d881400addd7c143a27d03
Leo Paul Larkin <lplinkl00@gmail.com>
2025-04-11 11:49:46 +0800

    Update models.py

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import func
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(255))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
 
